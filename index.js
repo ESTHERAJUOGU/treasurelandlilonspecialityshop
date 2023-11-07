fb5260a6733f7d66853804e2ef5925de9ee1748d
ESTHERAJUOGU <135168720+ESTHERAJUOGU@users.noreply.github.com>
2023-11-07 18:27:27 +0100

    updated index.html, style.css and index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+
+function bookNow(){
+    let button = prompt("do you want to book now?");
+}
+let buttonElement=document.querySelector("#button");
+buttonElement.addEventListener("click",bookNow);
